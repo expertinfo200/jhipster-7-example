6c5ee41e9e9a2d5b68434e50e7f1ea4090700a41
unknown <mskeszlrd@gmail.com>
2021-11-15 05:16:56 +0100

    initial commit

---
+++ src/test/javascript/cypress/integration/entity/post.spec.ts
@@ -0,0 +1,177 @@
+import { entityItemSelector } from '../../support/commands';
+import {
+  entityTableSelector,
+  entityDetailsButtonSelector,
+  entityDetailsBackButtonSelector,
+  entityCreateButtonSelector,
+  entityCreateSaveButtonSelector,
+  entityCreateCancelButtonSelector,
+  entityEditButtonSelector,
+  entityDeleteButtonSelector,
+  entityConfirmDeleteButtonSelector,
+} from '../../support/entity';
+
+describe('Post e2e test', () => {
+  const postPageUrl = '/post';
+  const postPageUrlPattern = new RegExp('/post(\\?.*)?$');
+  const username = Cypress.env('E2E_USERNAME') ?? 'admin';
+  const password = Cypress.env('E2E_PASSWORD') ?? 'admin';
+  const postSample = { title: 'B2C', content: 'Li4vZmFrZS1kYXRhL2Jsb2IvaGlwc3Rlci50eHQ=', date: '2021-11-13T01:26:42.555Z' };
+
+  let post: any;
+
+  before(() => {
+    cy.window().then(win => {
+      win.sessionStorage.clear();
+    });
+    cy.visit('');
+    cy.login(username, password);
+    cy.get(entityItemSelector).should('exist');
+  });
+
+  beforeEach(() => {
+    cy.intercept('GET', '/api/posts+(?*|)').as('entitiesRequest');
+    cy.intercept('POST', '/api/posts').as('postEntityRequest');
+    cy.intercept('DELETE', '/api/posts/*').as('deleteEntityRequest');
+  });
+
+  afterEach(() => {
+    if (post) {
+      cy.authenticatedRequest({
+        method: 'DELETE',
+        url: `/api/posts/${post.id}`,
+      }).then(() => {
+        post = undefined;
+      });
+    }
+  });
+
+  it('Posts menu should load Posts page', () => {
+    cy.visit('/');
+    cy.clickOnEntityMenuItem('post');
+    cy.wait('@entitiesRequest').then(({ response }) => {
+      if (response!.body.length === 0) {
+        cy.get(entityTableSelector).should('not.exist');
+      } else {
+        cy.get(entityTableSelector).should('exist');
+      }
+    });
+    cy.getEntityHeading('Post').should('exist');
+    cy.url().should('match', postPageUrlPattern);
+  });
+
+  describe('Post page', () => {
+    describe('create button click', () => {
+      beforeEach(() => {
+        cy.visit(postPageUrl);
+        cy.wait('@entitiesRequest');
+      });
+
+      it('should load create Post page', () => {
+        cy.get(entityCreateButtonSelector).click({ force: true });
+        cy.url().should('match', new RegExp('/post/new$'));
+        cy.getEntityCreateUpdateHeading('Post');
+        cy.get(entityCreateSaveButtonSelector).should('exist');
+        cy.get(entityCreateCancelButtonSelector).click({ force: true });
+        cy.wait('@entitiesRequest').then(({ response }) => {
+          expect(response!.statusCode).to.equal(200);
+        });
+        cy.url().should('match', postPageUrlPattern);
+      });
+    });
+
+    describe('with existing value', () => {
+      beforeEach(() => {
+        cy.authenticatedRequest({
+          method: 'POST',
+          url: '/api/posts',
+          body: postSample,
+        }).then(({ body }) => {
+          post = body;
+
+          cy.intercept(
+            {
+              method: 'GET',
+              url: '/api/posts+(?*|)',
+              times: 1,
+            },
+            {
+              statusCode: 200,
+              body: [post],
+            }
+          ).as('entitiesRequestInternal');
+        });
+
+        cy.visit(postPageUrl);
+
+        cy.wait('@entitiesRequestInternal');
+      });
+
+      it('detail button click should load details Post page', () => {
+        cy.get(entityDetailsButtonSelector).first().click();
+        cy.getEntityDetailsHeading('post');
+        cy.get(entityDetailsBackButtonSelector).click({ force: true });
+        cy.wait('@entitiesRequest').then(({ response }) => {
+          expect(response!.statusCode).to.equal(200);
+        });
+        cy.url().should('match', postPageUrlPattern);
+      });
+
+      it('edit button click should load edit Post page', () => {
+        cy.get(entityEditButtonSelector).first().click();
+        cy.getEntityCreateUpdateHeading('Post');
+        cy.get(entityCreateSaveButtonSelector).should('exist');
+        cy.get(entityCreateCancelButtonSelector).click({ force: true });
+        cy.wait('@entitiesRequest').then(({ response }) => {
+          expect(response!.statusCode).to.equal(200);
+        });
+        cy.url().should('match', postPageUrlPattern);
+      });
+
+      it('last delete button click should delete instance of Post', () => {
+        cy.get(entityDeleteButtonSelector).last().click();
+        cy.getEntityDeleteDialogHeading('post').should('exist');
+        cy.get(entityConfirmDeleteButtonSelector).click({ force: true });
+        cy.wait('@deleteEntityRequest').then(({ response }) => {
+          expect(response!.statusCode).to.equal(204);
+        });
+        cy.wait('@entitiesRequest').then(({ response }) => {
+          expect(response!.statusCode).to.equal(200);
+        });
+        cy.url().should('match', postPageUrlPattern);
+
+        post = undefined;
+      });
+    });
+  });
+
+  describe('new Post page', () => {
+    beforeEach(() => {
+      cy.visit(`${postPageUrl}`);
+      cy.get(entityCreateButtonSelector).click({ force: true });
+      cy.getEntityCreateUpdateHeading('Post');
+    });
+
+    it('should create an instance of Post', () => {
+      cy.get(`[data-cy="title"]`).type('action-items Omani').should('have.value', 'action-items Omani');
+
+      cy.get(`[data-cy="content"]`)
+        .type('../fake-data/blob/hipster.txt')
+        .invoke('val')
+        .should('match', new RegExp('../fake-data/blob/hipster.txt'));
+
+      cy.get(`[data-cy="date"]`).type('2021-11-13T02:51').should('have.value', '2021-11-13T02:51');
+
+      cy.get(entityCreateSaveButtonSelector).click();
+
+      cy.wait('@postEntityRequest').then(({ response }) => {
+        expect(response!.statusCode).to.equal(201);
+        post = response!.body;
+      });
+      cy.wait('@entitiesRequest').then(({ response }) => {
+        expect(response!.statusCode).to.equal(200);
+      });
+      cy.url().should('match', postPageUrlPattern);
+    });
+  });
+});
